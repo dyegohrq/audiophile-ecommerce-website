f68c95a42bc4563afe8d7e72a6aaef0a4703efe0
José Dyego Henrique Cardoso de Sousa <116648398+dyegohrq@users.noreply.github.com>
2024-04-04 21:02:16 -0300

    ;

--- script/script.js
+++ script/script.js
@@ -18,7 +18,6 @@ let number = document.querySelector(".number");
 let more = document.querySelector(".more");
 
 let cartArray = [];
-let productArray = [];
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
@@ -57,6 +56,114 @@ anyLess.addEventListener("click", () => {
     number.value = parseInt(number.value) - 1;
   }
 });
+
+class Bd {
+  constructor(){
+    let id = localStorage.getItem('id')
+
+    if (id === null) {
+      localStorage.setItem('id', 0)
+    }
+  }
+
+  getProximoId(){
+    let proximoId = localStorage.getItem('id')
+
+    return parseInt(proximoId) + 1
+  }
+
+  addItem(d) {
+    let id = this.getProximoId();
+    localStorage.setItem('id', id)
+    localStorage.setItem(id , JSON.stringify(d))
+  }
+
+  recuperarProduto(){
+    let productArray = Array();
+    let id = localStorage.getItem('id');
+
+    for(let i = 1; i <= id; i++){
+      let produtoObj = JSON.parse(localStorage.getItem(i));
+
+      if (produtoObj === null) {
+        continue
+      }
+      productArray.push(produtoObj)
+    }
+
+    return productArray
+  }
+}
+
+let bd  = new Bd();
+
+
+productDetail.addEventListener('click', (e) => {
+  // e.preventDefault();
+
+  let parentButton = e.target.closest('.add-cart')
+
+  if (parentButton) {
+    let name = parentButton.getAttribute('data-name')
+    let price = parseFloat(parentButton.getAttribute('data-price'))
+
+    let getItem = new GetItem(name, price, parseInt(number.value))
+
+    bd.addItem(getItem)
+    
+  }
+})
+
+class GetItem {
+  constructor(name, price, number) {
+    this.name = name,
+    this.price = price,
+    this.number = number
+  }
+}
+
+function carregarProduto() {
+  let productArray = Array()
+  
+  productArray = bd.recuperarProduto()
+
+  updateCard(productArray)
+}
+
+function updateCard(productArray) {
+  cartMain.innerHTML = "";
+  let total = 0;
+
+  productArray.forEach((item) => {
+    let line = cartMain.insertRow()
+
+    line.insertCell(0).innerHTML = '<div class="img-card"></div>'
+    line.insertCell(1).innerHTML = `
+    <div class="card-product-name"
+      <div class="name-product">
+        ${item.name}
+      </div>
+      <div class="price">
+        $ ${item.price}
+      </div>
+    </div>
+    `
+    line.insertCell(2).innerHTML = `
+    <div class="add-to-card">
+      <div class="amount amout-card">
+        <span class="btn-add any-less">-</span>
+        <input type="number" value='${item.number}' class="number"></input>
+        <span class="btn-add more">+</span>
+      </div>
+    </div>
+    `
+  });
+
+  cardTotal.textContent = "$ " + total;
+  cardCountry.innerText = cartArray.length;
+}
+
+/*
 
 productDetail.addEventListener("click", (event) => {
   event.preventDefault();
@@ -113,49 +220,36 @@ function addToCard(name, price, number) {
   updateCard();
 }
 
-function updateCard() {
-  cartMain.innerHTML = "";
-  let total = 0;
 
-  cartArray.forEach((item) => {
-    cardProduct.classList.add("card-main-product");
-    if (item.price > 999) {
-      item.price.toFixed(3);
-    }
-
-    cardProduct.innerHTML = `
-      <div class="img-card"></div>
-      <div class="card-product-name">
-        <div class="name-product">
-          ${item.name}
-        </div>
-        <div class="price">
-          $ ${item.price}
-        </div>
-      </div>
-      <div class="add-to-card">
-        <div class="amount amout-card">
-          <span class="btn-add any-less">-</span>
-          <input type="number" value='${item.number}' class="number"></input>
-          <span class="btn-add more">+</span>
-        </div>
-      </div>
-    `;
-
-    total += item.price * item.number;
-
-    if (total > 999) {
-      total.toFixed(3);
-    }
-    cartMain.appendChild(cardProduct);
-  });
-
-  cardTotal.textContent = "$ " + total;
-  cardCountry.innerText = cartArray.length;
-}
 
 removeAll.addEventListener("click", (e) => {
   if (cardProduct.remove()) {
     window.location.reload(true);
   }
 });
+*/
+
+// cardProduct.classList.add("card-main-product");
+    
+    // cardProduct.innerHTML = `
+    //   <div class="img-card"></div>
+    //   <div class="card-product-name">
+    //     <div class="name-product">
+    //       ${item.name}
+    //     </div>
+    //     <div class="price">
+    //       $ ${item.price}
+    //     </div>
+    //   </div>
+      // <div class="add-to-card">
+      //   <div class="amount amout-card">
+      //     <span class="btn-add any-less">-</span>
+      //     <input type="number" value='${item.number}' class="number"></input>
+      //     <span class="btn-add more">+</span>
+      //   </div>
+      // </div>
+    // `;
+
+    // total += item.price * item.number;
+
+    // cartMain.appendChild(cardProduct);
