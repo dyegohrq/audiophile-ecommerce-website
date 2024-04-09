41cd6ad93e4a64f18303ca91c0fab5fa9156646e
José Dyego Henrique Cardoso de Sousa <116648398+dyegohrq@users.noreply.github.com>
2024-04-09 18:35:36 -0300

    .

--- script/script.js
+++ script/script.js
@@ -17,6 +17,8 @@ let anyLess = document.querySelector(".any-less");
 let number = document.querySelector(".number");
 let more = document.querySelector(".more");
 
+let imgCard = document.querySelector('.img-card')
+
 let cartArray = [];
 
 button.addEventListener("click", (e) => {
@@ -132,126 +134,14 @@ function carregarProduto() {
 
 function updateCard(productArray) {
   cartMain.innerHTML = "";
-
-
   let total = 0;
 
-  productArray.forEach((item) => {
-    let line = cartMain.insertRow()
-
-    line.insertCell(0).innerHTML = '<div class="img-card"></div>'
-    line.insertCell(1).innerHTML = `
-    <div class="card-product-name"
-      <div class="name-product">
-        ${item.name}
-      </div>
-      <div class="price">
-        $ ${item.price}
-      </div>
-    </div>
-    `
-    line.insertCell(2).innerHTML = `
-    <div class="add-to-card">
-      <div class="amount amout-card">
-        <span class="btn-add any-less">-</span>
-        <input type="number" value='${item.number}' class="number"></input>
-        <span class="btn-add more">+</span>
-      </div>
-    </div>
-    `
+  productArray.forEach(item => {
+    switch (item.name) {
+      case 'XX59':
+        imgCard.style.backgroundImage = 'url(../assets/product-xx59-headphones/mobile/image-product.jpg)'
+        break;
+    }
   });
-
-  cardTotal.textContent = "$ " + total;
-  cardCountry.innerText = cartArray.length;
-}
-
-/*
-
-productDetail.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  // console.log(event.target)
-  let parentButton = event.target.closest(".add-cart");
-
-  if (parentButton) {
-    let name = parentButton.getAttribute("data-name");
-    let price = parseFloat(parentButton.getAttribute("data-price"));
-
-    addToCard(name, price, parseInt(number.value));
-    addItem(name, price, parseInt(number.value));
-  }
-});
-
-function addItem(name, price, number) {
-  cartArray = [
-    {
-      name,
-      price,
-      number,
-    },
-  ];
-
-
-  if (localStorage.nameI) {
-    productArray = JSON.parse(localStorage.getItem("name"));
-  }
-
-
-  let novoItem = cartArray;
-  productArray.push(novoItem);
-  localStorage.nameI = JSON.stringify(productArray);
-
-  console.log(productArray.length);
-}
-
-function addToCard(name, price, number) {
-  let exigistingItem = cartArray.find((item) => item.name === name);
-
-  if (exigistingItem) {
-    exigistingItem.number += 1;
-  } else {
-    cartArray = [
-      {
-        name,
-        price,
-        number,
-      },
-    ];
-  }
-
-  updateCard();
-}
-
-
-
-removeAll.addEventListener("click", (e) => {
-  if (cardProduct.remove()) {
-    window.location.reload(true);
-  }
-});
-*/
-
-// cardProduct.classList.add("card-main-product");
-    
-    // cardProduct.innerHTML = `
-    //   <div class="img-card"></div>
-    //   <div class="card-product-name">
-    //     <div class="name-product">
-    //       ${item.name}
-    //     </div>
-    //     <div class="price">
-    //       $ ${item.price}
-    //     </div>
-    //   </div>
-      // <div class="add-to-card">
-      //   <div class="amount amout-card">
-      //     <span class="btn-add any-less">-</span>
-      //     <input type="number" value='${item.number}' class="number"></input>
-      //     <span class="btn-add more">+</span>
-      //   </div>
-      // </div>
-    // `;
-
-    // total += item.price * item.number;
-
-    // cartMain.appendChild(cardProduct);
+ 
+};
