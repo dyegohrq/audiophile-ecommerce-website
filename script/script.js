cd249bfae415d093d99eabab35bb0a09d34de264
José Dyego Henrique Cardoso de Sousa <116648398+dyegohrq@users.noreply.github.com>
2024-04-09 20:40:08 -0300

    .

--- script/script.js
+++ script/script.js
@@ -17,7 +17,7 @@ let anyLess = document.querySelector(".any-less");
 let number = document.querySelector(".number");
 let more = document.querySelector(".more");
 
-let imgCard = document.querySelector('.img-card')
+let addProduct = document.querySelector('.add-product');
 
 let cartArray = [];
 
@@ -34,19 +34,6 @@ function openMenu() {
   nav.style.position = "fixed";
 }
 
-card.addEventListener("click", () => {
-  cartContainer.classList.toggle("active-card");
-  cardProduct.classList.toggle("add-card");
-});
-
-cartContainer.addEventListener("mousedown", (event) => {
-  if (cart.contains(event.target)) {
-  } else {
-    cartContainer.classList.toggle("active-card");
-    cardProduct.classList.toggle("add-card");
-  }
-});
-
 more.addEventListener("click", () => {
   if (number.value < 10) {
     number.value = parseInt(number.value) + 1;
@@ -58,90 +45,3 @@ anyLess.addEventListener("click", () => {
     number.value = parseInt(number.value) - 1;
   }
 });
-
-class Bd {
-  constructor(){
-    let id = localStorage.getItem('id')
-
-    if (id === null) {
-      localStorage.setItem('id', 0)
-    }
-  }
-
-  getProximoId(){
-    let proximoId = localStorage.getItem('id')
-
-    return parseInt(proximoId) + 1
-  }
-
-  addItem(d) {
-    let id = this.getProximoId();
-    localStorage.setItem('id', id)
-    localStorage.setItem(id , JSON.stringify(d))
-  }
-
-  recuperarProduto(){
-    let productArray = Array();
-    let id = localStorage.getItem('id');
-
-    for(let i = 1; i <= id; i++){
-      let produtoObj = JSON.parse(localStorage.getItem(i));
-
-      if (produtoObj === null) {
-        continue
-      }
-      productArray.push(produtoObj)
-    }
-
-    return productArray
-  }
-}
-
-let bd  = new Bd();
-
-
-productDetail.addEventListener('click', (e) => {
-  // e.preventDefault();
-
-  let parentButton = e.target.closest('.add-cart')
-
-  if (parentButton) {
-    let name = parentButton.getAttribute('data-name')
-    let price = parseFloat(parentButton.getAttribute('data-price'))
-
-    let getItem = new GetItem(name, price, parseInt(number.value))
-
-    bd.addItem(getItem)
-    
-  }
-})
-
-class GetItem {
-  constructor(name, price, number) {
-    this.name = name,
-    this.price = price,
-    this.number = number
-  }
-}
-
-function carregarProduto() {
-  let productArray = Array()
-  
-  productArray = bd.recuperarProduto()
-
-  updateCard(productArray)
-}
-
-function updateCard(productArray) {
-  cartMain.innerHTML = "";
-  let total = 0;
-
-  productArray.forEach(item => {
-    switch (item.name) {
-      case 'XX59':
-        imgCard.style.backgroundImage = 'url(../assets/product-xx59-headphones/mobile/image-product.jpg)'
-        break;
-    }
-  });
- 
-};
