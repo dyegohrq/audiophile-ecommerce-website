f262b3b7664cef95880284f9c4decd3feadcee25
José Dyego Henrique Cardoso de Sousa <116648398+dyegohrq@users.noreply.github.com>
2025-04-14 11:37:48 -0300

    .

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,34 +1,46 @@
 import { FiShoppingCart } from 'react-icons/fi'
 import logoImg from '../../../public/assets/shared/desktop/logo.svg'
 import './index.css'
+import { Link } from 'react-router'
+import style from '../root.module.css'
 
 export function Header() {
     return(
         <header className='w-full h-[90px] flex items-center justify-center' >
-            <nav className='flex items-center justify-around w-full'>
-                <button className="burger" >
-                    <div className="hambuguer">
-                        <div className="bar" id="bar1"></div>
-                        <div className="bar" id="bar2"></div>
-                        <div className="bar" id="bar3"></div>
+            <nav className='flex items-center justify-around w-full max-w-[1110px] h-full'>
+                <div className="left_content w-[55%] flex items-center justify-between md:justify-start gap-[42px] lg:w-auto">
+                    <button className="burger cursor-pointer lg:hidden" >
+                        <div className="hambuguer">
+                            <div className="bar" id="bar1"></div>
+                            <div className="bar" id="bar2"></div>
+                            <div className="bar" id="bar3"></div>
+                        </div>
+                    </button>
+                    <div className="logo">
+                        <Link to={'/'} ><img src={logoImg} alt="" /></Link>
                     </div>
-                </button>
-                <div className="logo">
-                    <img src={logoImg} alt="" />
                 </div>
-                <FiShoppingCart size={20} color='#fff' />
+                <FiShoppingCart size={20} color='#fff' className='icon order-2 cursor-pointer'/>
 
-                <ul className="menu_mobile hidden">
+                <ul className="menu_mobile absolute bg-white w-full h-full lg:hidden">
                     Home
                     Headphones
                     Speakers
                     Earphones
                 </ul>
-                <ul className="menu_descktop hidden">
-                    Home
-                    Headphones
-                    Speakers
-                    Earphones
+                <ul className={ `${style.subtitle} menu_descktop hidden order-1 gap-[34px] text-white lg:flex` }>
+                    <li className="list_item">
+                        <Link to={'/'}>Home</Link>
+                    </li>
+                    <li className="list_item">
+                        <Link to={'/'}>Headphones</Link>
+                    </li>
+                    <li className="list_item">
+                        <Link to={'/'}>Speakers</Link>
+                    </li>
+                    <li className="list_item">
+                        <Link to={'/'}>Earphones</Link>
+                    </li>
                 </ul>
             </nav>
         </header>
